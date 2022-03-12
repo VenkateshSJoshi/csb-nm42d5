ebf654b5cd21a3d0afc62acb97b48533fc15f89e
Code Sandbox <hello@codesandbox.io>
2022-03-12 13:16:14 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.d7e05975546053a8a9e188a6ec5a8765.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21c72334d14c174176e68fb647a08b31",
+    "url": "https://venkateshsjoshi.github.io/csb-nm42d5/index.html"
+  },
+  {
+    "revision": "0127fc318bbea6553d10",
+    "url": "https://venkateshsjoshi.github.io/csb-nm42d5/static/css/main.aa773141.chunk.css"
+  },
+  {
+    "revision": "eea4e2e34839181b6760",
+    "url": "https://venkateshsjoshi.github.io/csb-nm42d5/static/js/2.a8a4c10f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "https://venkateshsjoshi.github.io/csb-nm42d5/static/js/2.a8a4c10f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0127fc318bbea6553d10",
+    "url": "https://venkateshsjoshi.github.io/csb-nm42d5/static/js/main.1ecbc116.chunk.js"
+  },
+  {
+    "revision": "8516a0f0e8d978166bc7",
+    "url": "https://venkateshsjoshi.github.io/csb-nm42d5/static/js/runtime-main.12e91261.js"
+  }
+]);
